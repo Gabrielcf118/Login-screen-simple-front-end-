0e289d3b58d42638ee77ec2da9f45593f95b5b86
Gabriel Oliveira <gabrieloliveiracf118@gmail.com>
2023-11-12 13:54:53 -0300

    first commit

---
+++ constants/colors.js
@@ -0,0 +1,9 @@
+const COLORS = {
+  white: "#ffffff",
+  black: "#222222",
+  primary: "#007260",
+  secondary: "#39868D",
+  grey: "#cccccc",
+};
+
+export default COLORS;
---
+++ screens/index.js
@@ -0,0 +1,5 @@
+import Login from "./Login";
+import Signup from "./Signup";
+import Welcome from "./Welcome";
+
+export { Login, Welcome, Signup };
